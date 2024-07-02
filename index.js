209731e3b27b5121e995a2deb63798b658893192
Laurine Ndanu Nzuki <nzukilaurine@gmail.com>
2024-07-02 18:32:28 +0300

    Arithmetic labs passing

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
 // Write your code here
+//1
+let num1 = 62;
+let num2 = 1;
+
+let multiply = num1 * num2;
+console.log(multiply);
+
+//2
+let random = Math.floor(Math.random() * 10)+1;
+console.log(random);
+
+//3
+let num4 = 24;
+let num3 = 6;
+let mod = num4 / num3;
+console.log(mod);
+
+//4
+let max = Math.max (7, 9, 3, 11, 16, 20);
+console.log(max);
